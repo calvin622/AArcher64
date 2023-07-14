63dd40451be67ff80b5410b548bb56fd30e3c6d9
CalvinR <calvin.2000@outlook.com>
2023-07-14 09:18:06 +0000

    added regex matching for increased speed

--- aarcher64/analyse_gadgets.py
+++ aarcher64/analyse_gadgets.py
@@ -1,10 +1,10 @@
-import angr
 
+import re
 
 class Gadget:
     def __init__(self, address, instructions, length, controlled_registers, constraint_solutions):
         """
-        Represents a gadget with its address, instructions, length, and controlled registers.
+        Represents a gadget with its address, instructions, length, controlled registers, and constraints.
         """
         self.address = address
         self.instructions = list(instructions)
@@ -13,50 +13,31 @@ class Gadget:
         self.constraint_solutions = list(constraint_solutions)
 
 
-def analyse_controlled_registers(project, state, address):
+def analyse_controlled_registers(block):
     """
     Take a step forward in the simulation to reach the specified address.
     """
-    simgr = project.factory.simgr(state)
-    sp_before = state.regs.sp
-    changed_registers = []
+    registers = []
+    pattern = r"PUT\(([^)]+)\)"
+    matches = re.findall(pattern, str(block.vex))
 
-    simgr.step()
+    for match in matches:
+        if match.startswith("x") or match.startswith("v"):
+            registers.append(match)
 
-    final_state = simgr.active[0] if simgr.active else None
-    if final_state:
-        # Check for changed registers (this is slow)
-        changed_registers = get_changed_registers(final_state, state)
-
-        # Check if the sp register has changed
-        if state.solver.eval(final_state.regs.sp) != state.solver.eval(sp_before):
-            changed_registers.append('sp')
-
-    return changed_registers
-
-
-def get_changed_registers(final_state, state):
-    """
-    Check for changed registers between final_state and state.
-    """
-    return [
-        reg_name
-        for reg_name in [f'x{i}' for i in range(31)]
-        if state.solver.eval(final_state.regs.get(reg_name)) != state.solver.eval(state.regs.get(reg_name))
-    ]
-
-
+    return registers
+    
 def process_unconstrained_state(project, unconstrained_state):
     """
     Process an unconstrained state and extract gadgets from it.
     """
     instruction_addr = unconstrained_state.regs.ip
     addr = unconstrained_state.solver.eval(instruction_addr)
-    controlled_registers = analyse_controlled_registers(
-        project, unconstrained_state.copy(), instruction_addr)
-
+    
     constraint_solutions = []
     block_list = [create_block(project, addr)]
+
+    controlled_registers = analyse_controlled_registers(block_list[0])
 
     num_ins = block_list[0].instructions
 
@@ -65,9 +46,10 @@ def process_unconstrained_state(project, unconstrained_state):
             project, unconstrained_state.copy())
         for constraint in constraints:
             address = unconstrained_state.solver.eval(constraint.regs.ip)
-            block_list.append(create_block(project, address))
+            block = create_block(project, address)
+            block_list.append(block)
             constraint_solutions.append(solve_constraints(constraint))
-            controlled_registers.extend(analyse_controlled_registers(project, constraint, constraint.regs.ip))
+            controlled_registers.extend(analyse_controlled_registers(block))
         gadget = Gadget(hex(addr), block_list, num_ins, controlled_registers, constraint_solutions)
         return [gadget]
 
